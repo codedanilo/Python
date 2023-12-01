df28991fd9ce67b96da14964bd2ca61e3698f949
Matheus Beckerath <matheus.beckerath@hotmail.com>
2023-12-01 01:28:40 -0300

    Add main

--- PI003/Supermercado/main.py
+++ PI003/Supermercado/main.py
@@ -38,3 +38,34 @@ def consultar_preco(produtos):
             return
 
     print("Produto não encontrado.\n")
+
+def main():
+    produtos = []
+
+    while True:
+        print("Menu:")
+        print("1. Inserir novo produto")
+        print("2. Excluir produto cadastrado")
+        print("3. Listar todos os produtos")
+        print("4. Consultar preço de um produto")
+        print("0. Sair")
+
+        opcao = input("Escolha uma opção: ")
+
+        if opcao == "1":
+            inserir_produto(produtos)
+        elif opcao == "2":
+            excluir_produto(produtos)
+        elif opcao == "3":
+            listar_produtos(produtos)
+        elif opcao == "4":
+            consultar_preco(produtos)
+        elif opcao == "0":
+            print("Saindo do programa. Até mais!")
+            break
+        else:
+            print("Opção inválida. Tente novamente.\n")
+
+
+if __name__ == "__main__":
+    main()
